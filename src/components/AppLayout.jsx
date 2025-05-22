b4b710c92147de2d3de5e21fdfc89f37077fd5fe
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2025-05-22 14:18:52 +0530

    loves

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router";
 import Navbar from "./navbar/Navbar";
-import Themes from "./themes/Themes";
 
 const AppLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      {/* <Themes /> */}
     </>
   );
 };
